dfa56ccf4e96ce0801c2a32637fe4140742db8a6
Hassan Miiro <hassanm645@gmail.com>
2019-09-13 00:02:26 +0300

    set production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,52 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-module.exports = {
-    //entry: './src/playground/redux.js',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]}
-    ]},
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback:true
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    //const CSSExtract = new MiniCssExtractPlugin('styles.css');
+    return {
+            entry: './src/app.js',
+            output: {
+                path: path.join(__dirname, 'public'),
+                filename: 'bundle.js'
+            },
+            module: {
+                rules: [{
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },{
+                    test: /\.s?css$/,
+                    use: 
+                        [{
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                            sourceMap:true
+                            }
+                        },
+                        'css-loader',
+                        'sass-loader',
+                        ]
+                    
+                }]
+            },
+            plugins: [
+                //new webpack.IgnorePlugin(/^./locale$/ /moment$/),
+                new MiniCssExtractPlugin({
+                    filename: 'styles.css',
+      chunkFilename: 'id.css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    })
+            ],
+            devtool: isProduction ? 'source-map' : 'inline-source-map',
+            devServer: {
+                contentBase: path.join(__dirname, 'public'),
+                historyApiFallback:true
+            }
+        };
+        // loader
+        
+        
     }
-};
-// loader
 
